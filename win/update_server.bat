036ca92e7b3256e36bf7f72a7ebcb2730b253811
raul <raulssorban@gmail.com>
2023-04-12 01:55:58 +0200

    Update++

--- win/update_server.bat
+++ win/update_server.bat
@@ -1,21 +1,8 @@
 @echo off
 
-if "%1" EQU "" (
-	set TAG=develop
-) else (
-	set TAG=%1
-)
-
-if "%TAG%" EQU "production" (
-	set BUILD=Release
-) else (
-	SET BUILD=Debug
-)
-
 SET root=%cd%
 SET server=%root%\server
 SET steam=%root%\steam
-SET url=https://github.com/CarbonCommunity/Carbon.Core/releases/download/%TAG%_build/Carbon.Windows.%BUILD%.zip	
 SET steamCmd=https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip
 
 rem Download the server
